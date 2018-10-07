8c0cad3421f267017b18ca46b727b8a092a47ae0
Jernej Vivod <je.vivod@gmail.com>
2018-10-07 07:16:29 +0200

    Add comments

--- group_analysis.py
+++ group_analysis.py
@@ -1,5 +1,6 @@
 from naloga1 import HierarchicalClustering, read_file, get_labels
 import numpy as np
+import matplotlib.pyplot as plt
 ########################
 # Author: Jernej Vivod #
 ########################
@@ -11,7 +12,7 @@ labels = get_labels(DATA_FILE) 						# Get labels (2xn matrix) that maps each co
 hc = HierarchicalClustering(read_file(DATA_FILE)) 	# Create a HierarchicalClustering instance initialized with parsed data.
 
 # Get groups and create a dictionary where index of groups maps to its members
-hc.get_groups(9)
+hc.get_groups(11)
 hc.extract_group_members()
 
 # For each group in hc.groups compute how many points it gave to every country.
@@ -23,58 +24,90 @@ for group_index in hc.groups.keys():
 		sum_points = np.add(sum_points, hc.data[country])
 	sum_points = np.true_divide(sum_points, len(hc.groups[group_index]))
 	points_to_countries[group_index] = sum_points
-### TODO: Create a barplot (many on same plot) showing the number of points given to each country by each group.
 
 
-# TODO: For each group in hc.groups compute how many points it gave to every region.
+# For each group in hc.groups compute how many points it gave to every region. ##
+
 points_to_regions = dict()
 
+# Go over group indices.
 for group_index in hc.groups.keys():
-	sum_points = np.zeros(47, dtype = int)
-	for country in hc.groups[group_index]:
+	sum_points = np.zeros(47, dtype = int) 		# Create empty vector for computing the cummulative sums of points for each country.
+	for country in hc.groups[group_index]: 		# Compute commulative sums.
 		sum_points = np.add(sum_points, hc.data[country])
 
-	points = dict()
-	num_votes = dict()
+	points = dict() 							# points: dict that maps group index to dict of points ratios for each region.
 
-	for region in set(labels[1,:]):
+	for region in set(labels[1,:]): 			# Go over regions and initialize values pointed by region names (keys) to 0.
 		points[region] = 0
-		num_votes[region] = 0
 
-	for index, country in enumerate(labels[0, :]):
+	for index, country in enumerate(labels[0, :]): 		# Compute cummulative sum for each region
 		points[labels[1, index]] += sum_points[index]
-		num_votes[labels[1, index]] += 1
 
-	#for key in points.keys():
-	#	points[key] /= num_votes[key]
+	for key in points.keys(): 							# Compute ratios of points to each region.
+		points[key] /= sum(sum_points)
 	
-	points_to_regions[group_index] = points
+	points_to_regions[group_index] = points 			# Add computed dict to points_to_regions dict.
 
-### TODO: Create a barplot (many on same plot) showing the number of points given to each country by each group.	
-## USE THE plot_dict MODULE
 
-# TODO: For each group in hc.groups compute how many points it gave to every other group in hc.groups (make a nxn matrix and a heatmap).
+# For each group in hc.groups compute how many points it gave to every other group in hc.groups (make a nxn matrix and a heatmap).
 
 points_to_groups = dict()
 
+# Go over group indices.
 for group_index in hc.groups.keys():
-	sum_points = np.zeros(47, dtype = int)
-	for country in hc.groups[group_index]:
+	sum_points = np.zeros(47, dtype = int) 					# Create empty vector for computing the cummulative sums of points for each country.
+	for country in hc.groups[group_index]: 					# Compute cummulative sums.
 		sum_points = np.add(sum_points, hc.data[country])
 
-	points_to_other_groups = dict()
-	num_votes = dict()
-	for key in hc.groups.keys():
+	points_to_other_groups = dict() 						# points_to_other_groups: dict that maps group index to points ratios for each other group.
+	for key in hc.groups.keys(): 							# Initialize all values pointed by group indices to 0.
 		points_to_other_groups[key] = 0
-		num_votes[key] = 0
 
-	for index, country in enumerate(labels[0, :]):
+	for index, country in enumerate(labels[0, :]): 			# Compute commulative sum of points awarded to each group by group with index group_index.
 		for key in hc.groups.keys():
 			if country in hc.groups[key]:
 				points_to_other_groups[key] += sum_points[index]
-				num_votes[key] += 1
 
-	for key in points_to_other_groups.keys():
-		points_to_other_groups[key] /= num_votes[key]
+	for key in points_to_other_groups.keys(): 				# Compute ratios.
+		points_to_other_groups[key] / sum(sum_points)
 
-	points_to_groups[group_index] = points_to_other_groups
+	points_to_groups[group_index] = points_to_other_groups 	# Add computed dict to points_to_regions dict.
+
+
+# plot_dict: plot a dictionary as a bar plot with key on the x axis and value mapped by the key on the y axis.
+def plot_dict(data_dict, suptitle):
+	plt.figure() 												# Create new figure.
+	for i in range(1, len(data_dict.keys()) + 1): 				# Plot each dict entry in own subplot.
+		plt.subplot(3, 4, i)
+		plt.bar(range(len(data_dict[i-1])), list(data_dict[i-1].values())) 	# Plot values.
+
+		# If keys are of string type, split into multiply lines by replacing space characters with newline characters
+		if isinstance(list(data_dict[i-1].keys())[0], str):
+			labs = list(map(lambda x: x.replace(' ', '\n'), list(data_dict[i-1].keys())))
+		else:
+			labs = list(data_dict[i-1].keys())
+
+		# Add x axis ticks.
+		plt.xticks(range(len(data_dict[i-1])), labs, fontsize = 8)
+
+		# Add y labels to leftmost subplots.
+		if i in {1, 5, 9}:
+			plt.ylabel("proportion of points given")
+
+		plt.title("Group {0}".format(i - 1)) 					# Add title to subplot
+
+	# Adjust margins.
+	plt.subplots_adjust(left=0.05, bottom=None, right=0.98, top=None, wspace=None, hspace=0.3)
+	plt.suptitle(suptitle, fontsize = 25) 						# Add title to main plot.
+	plt.show() 													# Show plot.
+
+# Delete "not listed" key from data dict for points to regions by groups data dist (countries with region "not listed" only vote and do not participate).
+for key in points_to_regions.keys():
+	del points_to_regions[key]["not listed"]
+
+# Plot distributions of point ratios among regions for each group.
+plot_dict(points_to_regions, "Proportion of Points Given to Each Region by Each Group")
+
+# Plot distribution of point ratios among other groups for each groups.
+plot_dict(points_to_groups, "Proportion of Points Given to Each Group by Each Group")
--- plot_dict.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.bar(range(len(points_to_regions[7])), list(points_to_regions[7].values()), align='center')
-plt.xticks(range(len(points_to_regions[7])), list(points_to_regions[7].keys()))
-plt.show()
-
-plt.bar(range(len(points_to_groups[7])), list(points_to_groups[7].values()), align='center')
-plt.xticks(range(len(points_to_groups[7])), list(points_to_groups[7].keys()))
-plt.show()
